cc92ca9fe58e90396edbfa3fdbc83e5ed7a0d09e
Steffen <steffen.haulund@gmail.com>
2024-09-04 13:15:51 +0200

    tumbling calculator Component cleanup more

--- src/app/score/page.tsx
+++ src/app/score/page.tsx
@@ -1,15 +1,12 @@
-"use client"
-import React, { useContext } from 'react' 
-import { TumblingScoreContext } from '../components/Tabs'
+
+import React from 'react' 
+
 
 export default function Score() {
-  const score = useContext(TumblingScoreContext) // Replace MyContext with the actual context object
+
   return (
     <div>
       <h2>Score</h2>
-      <p>Team Round {score.teamRound.difficulty}</p>
-      <p>Round Two {score.roundTwo.difficulty}</p>
-      <p>Round Three {score.roundThree.difficulty}</p>
     </div>
   )
 }
