056d3e503d6b548282c14657f89bbb7901aad53d
Steffen <steffen.haulund@gmail.com>
2024-09-05 10:40:30 +0200

    update to tubling elements and seriescalculation

--- src/app/data/tumbling_basic_elements.ts
+++ src/app/data/tumbling_basic_elements.ts
@@ -26,14 +26,54 @@ export const tumblingBasic: tumblingElement[] = [
     id: "FwT004",
   },
   {
+    name: "tuckedSalto 1/2",
+    score: 0.3,
+    id: "FwT0041/2",
+  },
+  {
+    name: "tuckedSalto 1/1",
+    score: 0.4,
+    id: "FwT0041/1",
+  },
+  {
     name: "pikedSalto",
     score: 0.3,
     id: "FwT005",
   },
   {
+    name: "pikedSalto 1/2",
+    score: 0.4,
+    id: "FwT0051/2",
+  },
+  {
     name: "straightSalto",
     score: 0.4,
     id: "FwT006",
+  },
+  {
+    name: "straightSalto 1/2",
+    score: 0.5,
+    id: "FwT0061/2",
+  },
+  {
+    name: "straightSalto 1/1",
+    score: 0.6,
+    id: "FwT0061/1",
+  },
+  {
+    name: "straightSalto 1 1/2",
+    score: 0.7,
+    id: "FwT00611/2",
+  },
+  {
+    name: "straightSalto 2/1",
+    score: 0.8,
+    id: "FwT0062/1",
+  },
+  {
+    name: "straightSalto 2 1/2",
+    score: 0.9,
+    id: "FwT00621/2",
   },
   {
     name: "startTuckedSalto",
@@ -56,14 +96,44 @@ export const tumblingBasic: tumblingElement[] = [
     id: "FwT010",
   },
   {
+    name: "doubleTuckedSalto 1/2",
+    score: 1.3,
+    id: "FwT0101/2",
+  },
+  {
+    name: "doubleTuckedSalto 1 1/2",
+    score: 1.5,
+    id: "FwT01011/2",
+  },
+  {
     name: "doublePikedSalto",
     score: 1.4,
     id: "FwT011",
   },
   {
+    name: "doublePikedSalto 1/2",
+    score: 1.5,
+    id: "FwT0111/2",
+  },
+  {
     name: "doubleStraightSalto",
     score: 1.6,
     id: "FwT012",
+  },
+  {
+    name: "doubleStraightSalto 1/2",
+    score: 1.7,
+    id: "FwT0121/2",
+  },
+  {
+    name: "doubleStraightSalto 1 1/2",
+    score: 1.9,
+    id: "FwT01211/2",
+  },
+  {
+    name: "doubleStraightSalto 2 1/2",
+    score: 2.1,
+    id: "FwT01221/2",
   },
   {
     name: "roundoff",
@@ -81,14 +151,59 @@ export const tumblingBasic: tumblingElement[] = [
     id: "BwT003",
   },
   {
+    name: "backtuck 1/2",
+    score: 0.3,
+    id: "BwT0031/2",
+  },
+  {
+    name: "backtuck 1/1",
+    score: 0.4,
+    id: "BwT0031/1",
+  },
+  {
     name: "backpiked",
     score: 0.2,
     id: "BwT004",
   },
   {
-    name: "backstraight",
+    name: "backpiked 1/2",
     score: 0.3,
+    id: "BwT0041/2",
+  },
+  {
+    name: "backstraight",
+    score: 0.4,
     id: "BwT005",
+  },
+  {
+    name: "backstraight 1/2",
+    score: 0.5,
+    id: "BwT0051/2",
+  },
+  {
+    name: "backstraight 1/1",
+    score: 0.6,
+    id: "BwT0051/1",
+  },
+  {
+    name: "backstraight 1 1/2",
+    score: 0.7,
+    id: "BwT00511/2",
+  },
+  {
+    name: "backstraight 2/1",
+    score: 0.8,
+    id: "BwT0052/1",
+  },
+  {
+    name: "backstraight 2 1/2",
+    score: 0.9,
+    id: "BwT00521/2",
+  },
+  {
+    name: "backstraight 3/1",
+    score: 1.0,
+    id: "BwT0053/1",
   },
   {
     name: "whipback",
@@ -101,6 +216,16 @@ export const tumblingBasic: tumblingElement[] = [
     id: "BwT007",
   },
   {
+    name: "doubleBacktuck 1/1",
+    score: 1.0,
+    id: "BwT0071/1",
+  },
+  {
+    name: "doubleBacktuck 2/1",
+    score: 1.2,
+    id: "BwT0072/1",
+  },
+  {
     name: "doubleBackpiked",
     score: 0.9,
     id: "BwT008",
@@ -111,13 +236,33 @@ export const tumblingBasic: tumblingElement[] = [
     id: "BwT009",
   },
   {
-    name: "tripleBacktuck",
+    name: "doubleBackstraight 1/1",
+    score: 1.4,
+    id: "BwT0091/1",
+  },
+  {
+    name: "doubleBackstraight 2/1",
     score: 1.6,
+    id: "BwT0092/1",
+  },
+  {
+    name: "doubleBackstraight 3/1",
+    score: 1.9,
+    id: "BwT0093/1",
+  },
+  {
+    name: "tripleBacktuck",
+    score: 1.7,
     id: "BwT010",
   },
   {
+    name: "tripleBacktuck 1/1",
+    score: 2.1,
+    id: "BwT0101/1",
+  },
+  {
     name: "tripleBackpiked",
-    score: 1.9,
+    score: 2.0,
     id: "BwT011",
   },
 ];
--- src/app/utils/utility.ts
+++ src/app/utils/utility.ts
@@ -2,61 +2,69 @@ import { disciplineScore } from "../components/Tabs"
 import { tumblingBasic, tumblingElement } from "../data/tumbling_basic_elements"
 
 
-export function getCalculatedTeamRoundScore(score: disciplineScore){
-    let result = 0
-  
-    // this from store
-    let elements = score.teamRound.series
-    
-    elements.forEach(el => {
-      let element : tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
+export function getCalculatedTeamRoundScore(score: disciplineScore) {
+  let result = 0
+
+  // this from store
+  let elements = score.teamRound.series
+
+  elements.forEach(el => {
+    let element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
+    if (element) {
+      result += element.score
+    }
+  })
+  return result * 6;
+}
+
+
+export function getCalculatedRoundTwoScore(score: disciplineScore) {
+  let result = 0
+
+  Object.values(score.roundTwo).forEach(series => {
+    series.forEach(el => {
+      let element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
       if (element) {
         result += element.score
       }
     })
-    return result*6;
-  }
-
-  
-export function getCalculatedRoundTwoScore(score: disciplineScore) {
-    let result = 0
-  
-    Object.values(score.roundTwo).forEach(series => {
-      series.forEach(el => {
-        let element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
-        if (element) {
-          result += element.score
-        }
-      })
-    })
-    return result;
-  }
+  })
+  return result;
+}
 
 
 
 
-  export function getCalculatedRoundThreeScore(score: disciplineScore) {
-    let result = 0
-  
-    Object.values(score.roundThree).forEach(series => {
-      series.forEach(el => {
-        let element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
-        if (element) {
-          result += element.score
-        }
-      })
-    })
-    return result;
-  }
+export function getCalculatedRoundThreeScore(score: disciplineScore) {
+  let result = 0
 
-  export function getCalculatedRoundScoreFromSeries({series}: {series: string[]}) {
-    let result = 0
-
+  Object.values(score.roundThree).forEach(series => {
     series.forEach(el => {
-        let element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
-        if (element) {
-            result += element.score
-        }
+      let element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el)
+      if (element) {
+        result += element.score
+      }
     })
-    return result;
+  })
+  return result;
+}
+
+export function getCalculatedRoundScoreFromSeries({ series }: { series: string[] }) {
+  const scoreArray: number[] = series.map(el => {
+    const element: tumblingElement | undefined = tumblingBasic.find(element => element.id === el);
+    return element?.score ?? 0;
+  });
+
+  const sortedArray = scoreArray.sort((a, b) => b - a);
+
+  let result = 0;
+
+  if (sortedArray.length >= 2) {
+    return result = sortedArray[0] + sortedArray[1];
+  }
+  if (sortedArray.length) {
+    return result = sortedArray[0];
+  }
+
+  return result;
 }
